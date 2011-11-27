907f05978673560e890b126e1b04896e60bb9157
Lysender <dc.eros@gmail.com>
2011-11-27 15:37:06 +0800

    Added sprint name generator

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -74,7 +74,7 @@ define('GENERIC_SALT', 'dJkrTa12s9as200d0783dss');
 /**
  * Defines the version of the application
  */
-define('APP_VERSION', '0.2.0');
+define('APP_VERSION', '0.2.1');
 
 /**
  * Initialize Kohana, setting the default options.
