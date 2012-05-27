be7d6801e4d276cf758e92354ec47dff467f0f0b
Lysender <dc.eros@gmail.com>
2012-05-27 21:42:09 +0800

    Bumped version

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -74,7 +74,7 @@ define('GENERIC_SALT', 'dJkrTa12s9as200d0783dss');
 /**
  * Defines the version of the application
  */
-define('APP_VERSION', '0.8.0');
+define('APP_VERSION', '0.9.0');
 
 /**
  * Initialize Kohana, setting the default options.
